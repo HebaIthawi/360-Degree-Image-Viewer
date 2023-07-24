af115ccc0d005de032a7ff73efcd5201c3e07042
HebaIthawi <heba.a.d@ihcoedu.uobaghdad.edu.iq>
2023-07-24 13:10:59 +0300

    2 panorama  images

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ const viewer = new PANOLENS.Viewer({
 });
 
 viewer.add(panoramaImage);
+
+
+const panoramaImage2 = new PANOLENS.ImagePanorama("images/image1.jpeg");
+const imageContainer2 = document.querySelector(".image-container2");
+
+const viewer2 = new PANOLENS.Viewer({
+  container: imageContainer2,
+  autoRotate: true,
+  autoRotateSpeed: 0.3,
+  controlBar: false,
+});
+
+viewer2.add( panoramaImage2);
