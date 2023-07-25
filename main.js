e29f030bfb5e628521d002a659d001d8260e0b52
HebaIthawi <heba.a.d@ihcoedu.uobaghdad.edu.iq>
2023-07-25 11:59:11 +0300

    multiple images

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const viewer = new PANOLENS.Viewer({
 viewer.add(panoramaImage);
 
 
-const panoramaImage2 = new PANOLENS.ImagePanorama("images/image1.jpeg");
+const panoramaImage2 = new PANOLENS.ImagePanorama("images/360-library.jpg");
 const imageContainer2 = document.querySelector(".image-container2");
 
 const viewer2 = new PANOLENS.Viewer({
@@ -21,4 +21,4 @@ const viewer2 = new PANOLENS.Viewer({
   controlBar: false,
 });
 
-viewer2.add( panoramaImage2);
+viewer2.add(panoramaImage2);
